28357f44d47d36f5df982a65e103ddd8980074a3
Glitch (glitch-hello-sqlite) <none>
2022-08-26 17:10:56 +0000

    🌁📇 Checkpoint ./src/sqlite.js:33974708/39

--- src/sqlite.js
+++ src/sqlite.js
@@ -24,11 +24,11 @@ await db.run("INSERT INTO Users (email,username,password,session_id,points,colle
 await db.run("INSERT INTO Users (email,username,password,session_id,points,collected,lat,long) VALUES ('shriramarvinth3012@gmail.com','Shriram','pass1',null,200, 1,null,null)");
 await db.run("INSERT INTO Users (email,username,password,session_id,points,collected,lat,long) VALUES ('rohithhtihor786@gmail.com','Rohith','pass1',null,100, 3,null,null)");
         
-        await db.run("CREATE TABLE Inventory (uid INTEGER, tid INTEGER)");
+        await db.run("CREATE TABLE TreesAttri (uid INTEGER, tid INTEGER)");
         
         
         await db.run("CREATE TABLE Inventory (uid INTEGER, tree_name)");
-        await db.run("INSERT INTO INVENTORY (uid,tid) VALUES (1,'oak tree'), (1,''), (1,3)")
+        await db.run("INSERT INTO INVENTORY (uid,tid) VALUES (1,'oak tree'), (1,'neem tree'), (1,'pine tree')")
         
       } else {
         console.log("db exists!")
