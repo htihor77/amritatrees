025814ddab2c5ef353ac22cc98f3eafd8928b0c2
Glitch (glitch-hello-sqlite) <none>
2022-08-30 17:46:48 +0000

    🍁🍛 Checkpoint ./src/partials/head.handlebars:33974708/1370 ./server.js:33974708/128 ./src/pages/index.hbs:33974708/1224 ./src/pages/inventory.hbs:33974708/1211 ./src/pages/leaderboards.hbs:33974708/2579 ./src/pages/login.hbs:33974708/7776 ./src/pages/signup.hbs:33974708/7475

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const fastify = require("fastify")({logger: false,});
 
 fastify.register(require("@fastify/static"), {root: path.join(__dirname, "public"),prefix: "/",});
 fastify.register(require("@fastify/formbody"));
-fastify.register(require("@fastify/view"), {engine: {handlebars: require("handlebars"),},});
+fastify.register(require("@fastify/view"), {
+  engine: {handlebars: require("handlebars"),},
+  options: {
+    partials:{
+      head: "/src/partials/head.handlebars",
+    }
+  },
+});
 
 // Load and parse SEO data
 const seo = require("./src/seo.json");
