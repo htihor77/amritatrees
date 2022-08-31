6cadbd5a2db7ba566be06f9c67019891ef262059
Glitch (glitch-hello-sqlite) <none>
2022-08-31 09:53:20 +0000

    🎮🔨 Checkpoint ./src/pages/index.hbs:33974708/177 ./src/sqlite.js:33974708/283 ./.glitch-assets:33974708/509

--- src/sqlite.js
+++ src/sqlite.js
@@ -28,6 +28,9 @@ await db.run("CREATE TABLE Users (uid INTEGER PRIMARY KEY AUTOINCREMENT, email T
         await db.run("CREATE TABLE aTREEbutes (tree_name TEXT PRIMARY KEY, value_points INTEGER, url TEXT)");
         await db.run("INSERT INTO aTREEbutes (tree_name,value_points,url) VALUES ('oak tree', 50, 'https://cdn.britannica.com/92/142292-004-459092B7.jpg'), ('neem tree', 50, 'https://5.imimg.com/data5/SELLER/Default/2021/5/RU/EL/LQ/76591799/neem-tree-500x500.jpg'), ('pine tree', 50, 'https://www.thetreecenter.com/c/uploads/2022/02/Slash_Pine_Tree_1-copy.jpg'), ('mango tree', 50, 'https://thumbs.dreamstime.com/b/mango-tree-isolate-white-background-mango-tree-isolate-white-108298705.jpg'), ('paradise tree', 100, 'https://1.bp.blogspot.com/-Xyjj17t6RbE/Tj_MlveMSCI/AAAAAAAAA6E/k2AqRAOpX-o/s1600/L1000264.JPG')");
         
+        //actually found trees in campus
+        await db.run("INSERT INTO aTREEbutes (tree_name,value_points,url) VALUES ('banyan tree',50,'https://cdn.discordapp.com/attachments/1014472011403239484/1014472226621378662/unknown.png')")
+        
         // await db.run("")
         
         
