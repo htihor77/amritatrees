4180bfb616c334af91ec2043d98a0321326b426d
Glitch (glitch-hello-sqlite) <none>
2022-09-06 12:27:39 +0000

    ☂️🎊 Checkpoint ./src/pages/inventory.hbs:33974708/286 ./public/css/style.css:33974708/749 ./.glitch-assets:33974708/455 ./server.js:33974708/72

--- server.js
+++ server.js
@@ -153,6 +153,9 @@ fastify.post("/signup", async (request, reply) => {
 
 
 fastify.get("/inventory", async (request, reply) => {
+  const params = request.query.raw;
+  console.log(params);
+  
   const user = request.session.user;
   const data = await db.runQuery1(`SELECT username,points FROM Users WHERE uid=${user.uid}`)
   const data1 = await db.runQuery1(`SELECT Inventory.tree_name,Atreebutes.url FROM Inventory,Atreebutes WHERE Inventory.uid=${user.uid} AND Inventory.tree_name=Atreebutes.tree_name`)
