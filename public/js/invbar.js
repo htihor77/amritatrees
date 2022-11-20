be4c34bf820898e02cb28bb58baf5096ab0666a3
Glitch (glitch-hello-sqlite) <none>
2022-11-20 13:19:49 +0000

    💅🦄 Checkpoint ./public/js/invbar.js:33974708/1440 ./src/partials/invbar.handlebars:33974708/764 ./src/pages/map.hbs:33974708/249 ./public/css/invbar.css:33974708/561

--- public/js/invbar.js
+++ public/js/invbar.js
@@ -1,8 +1,64 @@
 function toggleInventorybar(){
-  $('#invbar').classList.toggle('active');
+  $("#invbar").classList.toggle('active');
   if( $('#invbar').classList.contains("active") ){
     $("#overlay").style.display = "block";  
   } else {
     $("#overlay").style.display = "none";
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener("DOMContentLoaded", async ()=>{
+//   console.log("page loaded");
+//   const response = await fetch("./inventory?raw=json");
+//   const jsondata = await response.json();
+//   console.log(jsondata);
+
+//   if(response.ok != true){return;}
+  
+//   localStorage.setItem("inventoryData", JSON.stringify(jsondata) );
+//   const data = JSON.parse(localStorage.getItem("inventoryData"));
+  
+  
+//   $(".cards-container").innerHTML = "";
+//   data.unlocked.forEach(e=>{
+//     console.log(e);
+//     const card = document.createElement("div");
+//     card.classList = "card";
+//     card.innerHTML = `
+//               <div class="img"><img src="${e.url}"></div>
+//               <div class="label">${e.tree_name}</div>`
+    
+//     $(".cards-container").appendChild(card);
+//   })
+  
+//   data.locked.forEach(e=>{
+//     console.log(e);
+//     const card = document.createElement("div");
+//     card.classList = "card";
+//     card.style.setProperty("--theme-color","white");
+//     card.innerHTML = `
+//               <div class="img"><img src="${e.url}" style="filter: grayscale(100%);"></div>
+//               <div class="label">???</div>`
+    
+//     $(".cards-container").appendChild(card);
+//   })
+// })
