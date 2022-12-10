fe295abe624857cb43c509f0b6af09d4cb4d6cee
Glitch (glitch-hello-sqlite) <none>
2022-12-10 12:14:24 +0000

    🐪🚨 Checkpoint ./src/partials/invbar.handlebars:33974708/31 ./public/js/invbar.js:33974708/1145 ./public/css/invbar.css:33974708/4

--- public/js/invbar.js
+++ public/js/invbar.js
@@ -13,7 +13,42 @@ function toggleInventorybar(){
 
 
 
+async function fetchInventoryItems(){
+  console.log("page loaded");
+  const response = await fetch("./inventory?raw=json");
+  const jsondata = await response.json();
+  console.log(jsondata);
 
+  if(response.ok != true){return;}
+  
+  localStorage.setItem("inventoryData", JSON.stringify(jsondata) );
+  const data = JSON.parse(localStorage.getItem("inventoryData"));
+  
+  
+  $(".cards-container").innerHTML = "";
+  data.unlocked.forEach(e=>{
+    console.log(e);
+    const card = document.createElement("div");
+    card.classList = "card";
+    card.innerHTML = `
+              <div class="img"><img src="${e.url}"></div>
+              <div class="label">${e.tree_name}</div>`
+    
+    $(".cards-container").appendChild(card);
+  })
+  
+  data.locked.forEach(e=>{
+    console.log(e);
+    const card = document.createElement("div");
+    card.classList = "card";
+    card.style.setProperty("--theme-color","white");
+    card.innerHTML = `
+              <div class="img"><img src="${e.url}" style="filter: grayscale(100%);"></div>
+              <div class="label">???</div>`
+    
+    $(".cards-container").appendChild(card);
+  })
+}
 
 
 
