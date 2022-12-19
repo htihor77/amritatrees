1d59809cd65fb73c4822f28e5f944d6fa4edccdc
Glitch (glitch-hello-sqlite) <none>
2022-12-19 20:03:28 +0000

    🍲🍲 Checkpoint ./___quiz.json:33974708/900 ./src/sqlite.js:33974708/3

--- src/sqlite.js
+++ src/sqlite.js
@@ -50,7 +50,7 @@ dbWrapper
         
         // console.log(await db.all("SELECT * FROM Users"));
         // console.log(await db.all("SELECT * FROM Trees"));
-        // console.log(await db.all("SELECT * FROM A_TREE_butes"));
+        console.log(await db.all("SELECT * FROM A_TREE_butes"));
         // console.log(await db.all("SELECT * FROM Quiz"));
         
       }
