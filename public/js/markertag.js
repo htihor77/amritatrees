82ad3b71276fc584d878dd37e9854663dd10781a
Glitch (glitch-hello-sqlite) <none>
2023-09-08 20:37:06 +0000

    🙅🎎 Checkpoint ./public/js/markertag.js:33974708/784 ./src/partials/marker_prompt.handlebars:33974708/81

--- public/js/markertag.js
+++ public/js/markertag.js
@@ -1,4 +1,14 @@
+const zipperOpenSnd0 = new Audio("https://cdn.discordapp.com/attachments/1055426492659675167/1055434142420054016/zipper_open.mp3")
+const zipperCloseSnd0 = new Audio("https://cdn.discordapp.com/attachments/1055426492659675167/1055434191090761798/zipper_close.mp3")
 
 function addMarkerTag(){
   console.log("add new marker")
+  $("#marker_prompt").classList.add('active');
+  if ($("#soundCB").checked) zipperOpenSnd0.play();
+
+}
+
+function closeMarkerPrompt(){
+  $("#marker_prompt").classList.remove('active');
+  if ($("#soundCB").checked) zipperCloseSnd0.play();
 }
