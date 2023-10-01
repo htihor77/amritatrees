ad7806524c336dfa141a695f4191b329a3c2259a
Glitch (glitch-hello-sqlite) <none>
2023-10-01 15:14:56 +0000

    💄🐝 Checkpoint ./src/partials/marker_prompt.handlebars:33974708/26 ./public/js/markertag.js:33974708/1306

--- public/js/markertag.js
+++ public/js/markertag.js
@@ -13,11 +13,39 @@ function addMarkerTag(){
     return;
   }
 
-  
+  const videoElement = document.getElementById('camera-feed');
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Access the user's camera
+      navigator.mediaDevices.getUserMedia({ video: {facingMode: 'environment'} })
+          .then(function (stream) {
+              // Attach the camera stream to the video element
+              videoElement.srcObject = stream;
+          })
+          .catch(function (error) {
+              console.error('Error accessing camera:', error);
+          });
+  } else {
+      console.error('WebRTC is not supported in this browser.');
+  }
 
 }
 
 function closeMarkerPrompt(){
   $("#marker_prompt").classList.remove('active');
   if ($("#soundCB").checked) zipperCloseSnd0.play();
+  
+  const videoElement = document.getElementById('camera-feed');
+  const mediaStream = videoElement.srcObject;
+
+  if (mediaStream) {
+      // Stop all tracks in the media stream
+      mediaStream.getTracks().forEach(track => {
+          track.stop();
+      });
+
+      // Clear the video element's srcObject to stop displaying the stream
+      videoElement.srcObject = null;
+  } else {
+      console.error('No media stream to stop.');
+  }
 }
